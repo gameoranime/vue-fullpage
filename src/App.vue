6c8f955b3b1fe096d9485c05ae14f1946ac2897f
zengjiahao <250575102@qq.com>
2018-12-12 16:52:26 +0800

    添加了slides

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,11 @@
   <div id="app">
     <full-page ref="fullpage" :options="options">
       <div class="section">
-        <button class="next" @click="$refs.fullpage.api.moveSectionDown()">Next</button>
-        Section 1
+        
+        <div class="slide"><h1>Section 3<button class="next" @click="$refs.fullpage.api.moveSectionDown()">Next</button></h1></div>
+		<div class="slide"><h1>Slide 3.2</h1></div>
+		<div class="slide"><h1>Slide 3.3</h1></div>
+		<div class="slide"><h1>Slide 3.4</h1></div>
       </div>
       <div class="section">
         <button class="prev" @click="$refs.fullpage.api.moveSectionUp()">Prev</button>
@@ -26,7 +29,8 @@ export default {
       options: {
         menu: '#menu',
         anchors: ['page1', 'page2', 'page3'],
-        sectionsColor: ['#41b883', '#ff5f45', '#0798ec']
+        sectionsColor: ['#41b883', '#ff5f45', '#0798ec'],
+        scrollOverflow:true
       },
     }
   }
